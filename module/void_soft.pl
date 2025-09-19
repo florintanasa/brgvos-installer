d7142d0ad5654e191f4a6275c36130c5e892cade
Florin Tanasa <florin.tanasa@gmail.com>
2025-09-19 16:24:21 +0300

    Disables btrbk integration by default
    Comments out btrbk related code, effectively disabling its integration by default.
    
    This change ensures btrbk configuration is not automatically created, requiring manual setup for users who wish to utilize it.
    
    Improves snapper configuration by setting the correct group owner for /.snapshots and allowing the 'users' group access, enhancing usability and security.
    Also adds sed command to set user access to snapshots

--- module/void_soft.pl
+++ module/void_soft.pl
@@ -4,9 +4,9 @@
 % https://github.com/Oku-Code/void-linux-guide/blob/main/void-setup-guide.md
 
 % soft_info(Name, B, FS, PkgList, Descr).
-soft_info(btrbk, _, btrfs, [snooze, btrbk], 'Backup tool for btrfs subvolumes').
+%soft_info(btrbk, _, btrfs, [snooze, btrbk], 'Backup tool for btrfs subvolumes').
 soft_info('grub-btrfs', grub2, btrfs, ['grub-btrfs', 'grub-btrfs-runit'], 'Add a btrfs snapshots sub-menu to GRUB').
-% soft_info(snapper, _, btrfs, [snapper], 'Manage filesystem snapshots and allow undo of system modifications').
+soft_info(snapper, _, btrfs, [snapper], 'Manage filesystem snapshots and allow undo of system modifications').
 
 % Remove list of packages
 soft_remove_pkg_list(L, RD) :-
@@ -66,26 +66,26 @@ soft_install_deps_rd(_, [], _RD) :- !.
 soft_install_deps_rd(Pref, D, RD) :-
 	tui_progressbox_safe([Pref, 'xbps-install', o(r, RD), '-SyU', D, '2>&1'], '', [title(' Install Dependencies '), sz([12, 80])]).
 
-soft_configure(RD, snooze) :- !,
-	os_call2([chroot, RD, ln, '-sf', '/etc/sv/snooze-daily', '/var/service']),
-	os_call2([chroot, RD, ln, '-sf', '/etc/sv/snooze-hourly', '/var/service']),
-	true.
+%soft_configure(RD, snooze) :- !,
+%	os_call2([chroot, RD, ln, '-sf', '/etc/sv/snooze-daily', '/var/service']),
+%	os_call2([chroot, RD, ln, '-sf', '/etc/sv/snooze-hourly', '/var/service']),
+%	true.
 soft_configure(RD, 'grub-btrfs') :- !,
 	os_call2([chroot, RD, ln, '-sf', '/etc/sv/grub-btrfs', '/var/service']),
 	true.
-soft_configure(RD, btrbk) :- !,
-	os_mkdir_p(RD + '/mnt/btr_pool/btrbk_snapshots'),
-	atom_concat(RD, '/etc/btrbk/btrbk.conf', CF1),
-	open(CF1, write, S1),
-	soft_btrbk_write_cfg(S1),
-	close(S1),
+%soft_configure(RD, btrbk) :- !,
+%	os_mkdir_p(RD + '/mnt/btr_pool/btrbk_snapshots'),
+%	atom_concat(RD, '/etc/btrbk/btrbk.conf', CF1),
+%	open(CF1, write, S1),
+%	soft_btrbk_write_cfg(S1),
+%	close(S1),
 
-	atom_concat(RD, '/etc/cron.daily/btrbk', CF2),
-	open(CF2, write, S2),
-	soft_btrbk_write_cron(S2),
-	close(S2),
-	os_shell2([chmod, '711', CF2]),
-	true.
+%	atom_concat(RD, '/etc/cron.daily/btrbk', CF2),
+%	open(CF2, write, S2),
+%	soft_btrbk_write_cron(S2),
+%	close(S2),
+%	os_shell2([chmod, '711', CF2]),
+%	true.
 soft_configure(RD, snapper) :- !,
 	soft_update(RD),
 	os_call2([chroot, RD, ln, '-srf', '/etc/sv/dbus', '/var/service']),
@@ -94,39 +94,41 @@ soft_configure(RD, snapper) :- !,
 	os_call2([chroot, RD, snapper, '-c', root, 'create-config', '/']),
 	os_call2([chroot, RD, mkdir, '/.snapshots']),
 	os_call2([chroot, RD, chmod, '750', '/.snapshots']),
+	os_call2([chroot, RD, chown, ':users', '/.snapshots']),
+	os_call2([chroot, RD, sed, '-i', 's/ALLOW_GROUPS=""/ALLOW_GROUPS="users"/g', '/etc/default/grub']),
 	os_call2([chroot, RD, mount, '-a']),
 	os_call2([chroot, RD, ln, '-srf', '/etc/sv/snapperd', '/var/service']),
 	true.
 soft_configure(_RD, _) :-
 	true.
 
-soft_btrbk_write_cfg(S) :-
-	write(S, 'transaction_log            /var/log/btrbk.log'), nl(S),
-	write(S, 'timestamp_format           long'), nl(S),
-	write(S, 'stream_buffer              256m'), nl(S), nl(S),
+%soft_btrbk_write_cfg(S) :-
+%	write(S, 'transaction_log            /var/log/btrbk.log'), nl(S),
+%	write(S, 'timestamp_format           long'), nl(S),
+%	write(S, 'stream_buffer              256m'), nl(S), nl(S),
 
-	write(S, '# Configuration:'), nl(S),
-	write(S, 'snapshot_preserve_min   2d'), nl(S),
-	write(S, 'snapshot_preserve       14d'), nl(S), nl(S),
+%	write(S, '# Configuration:'), nl(S),
+%	write(S, 'snapshot_preserve_min   2d'), nl(S),
+%	write(S, 'snapshot_preserve       14d'), nl(S), nl(S),
 
-	write(S, 'target_preserve_min     no'), nl(S),
-	write(S, 'target_preserve         20d 10w *m'), nl(S), nl(S),
+%	write(S, 'target_preserve_min     no'), nl(S),
+%	write(S, 'target_preserve         20d 10w *m'), nl(S), nl(S),
 
-	write(S, 'archive_preserve_min    latest'), nl(S),
-	write(S, 'archive_preserve        12m 10y'), nl(S), nl(S),
+%	write(S, 'archive_preserve_min    latest'), nl(S),
+%	write(S, 'archive_preserve        12m 10y'), nl(S), nl(S),
 
-	write(S, 'volume /mnt/btr_pool'), nl(S),
-	write(S, '  snapshot_dir btrbk_snapshots'), nl(S), nl(S),
+%	write(S, 'volume /mnt/btr_pool'), nl(S),
+%	write(S, '  snapshot_dir btrbk_snapshots'), nl(S), nl(S),
 
-	write(S, '  subvolume @'), nl(S),
-	write(S, '  subvolume @opt'), nl(S),
-	write(S, '  subvolume @var'), nl(S),
-	write(S, '  subvolume @srv'), nl(S),
-	write(S, '  subvolume @home'), nl(S),
-	true.
+%	write(S, '  subvolume @'), nl(S),
+%	write(S, '  subvolume @opt'), nl(S),
+%	write(S, '  subvolume @var'), nl(S),
+%	write(S, '  subvolume @srv'), nl(S),
+%	write(S, '  subvolume @home'), nl(S),
+%	true.
 
-soft_btrbk_write_cron(S) :-
-	write(S, '#!/bin/sh'), nl(S),
-	write(S, 'exec /usr/bin/btrbk -q run'), nl(S),
-	true.
+%soft_btrbk_write_cron(S) :-
+%	write(S, '#!/bin/sh'), nl(S),
+%	write(S, 'exec /usr/bin/btrbk -q run'), nl(S),
+%	true.
 
